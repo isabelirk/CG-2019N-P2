92386ae49bcf68ef46b6c9e7f09bcb7520fa2c51
isabelirk <31291959+isabelirk@users.noreply.github.com>
2019-06-24 21:25:51 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,6 +9,10 @@
 #include <GL/gl.h>
 #include <iostream>
 
+const unsigned int FRAMES_POR_SEGUNDO = 30;
+const unsigned int UPDATE_INTERVAL_MS = 1000 / FRAMES_POR_SEGUNDO;
+
+GLint gTimeLastUpdateMs = 0; //variavel para controle do tempo junto com as constantes acima
 GLfloat rX = 0; //variavel para rotação eixo x
 GLfloat rY = 0; //variavel para rotação eixo y
 
@@ -16,7 +20,7 @@ GLfloat rY = 0; //variavel para rotação eixo y
 void display(){
 	glClearColor(0.4f, 0.4f, 0.4f, 1.0f); //definição da cor de fundo
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); //limpa a tela
-
+	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity(); // limpa transformações feitas anteriormente
 	
 
@@ -24,6 +28,12 @@ void display(){
 	glutSwapBuffers();
 }
 
+// função update - atualiza os desenhos da tela para mandar no display
+void update(int valor) {
+
+}
+
+// função teclado
 void keyboard(int key, int x, int y){
 	if (key == GLUT_KEY_RIGHT) {
 		rY += 5;
@@ -35,7 +45,16 @@ void keyboard(int key, int x, int y){
 		rX += 5;
 	}
 
+	glutPostRedisplay(); // solicita a atualização da display
+}
+
+// função update interna para auxiliar com o tempo - frames por segundo
+void internalUpdate(int value) {
+	int timeNowMs = glutGet(GLUT_ELAPSED_TIME);
+	double deltaSeconds = (timeNowMs - gTimeLastUpdateMs) / 1000.0;
+	gTimeLastUpdateMs = timeNowMs;
 	glutPostRedisplay();
+	glutTimerFunc(UPDATE_INTERVAL_MS, internalUpdate, 0);
 }
 
 int main(int argc, char **argv){
@@ -48,7 +67,9 @@ int main(int argc, char **argv){
 
 	//chamadas das funções principais
 	glutDisplayFunc(display);
+	glutTimerFunc(UPDATE_INTERVAL_MS, update, 0);
 	glutSpecialFunc(keyboard);
+	glutTimerFunc(UPDATE_INTERVAL_MS, internalUpdate, 0);
 
 	glutMainLoop(); //loop main
 	return 0;
